7bb0d5ca670b2481ad867a75b0bf5b8a9e0381c9
thuynguyen-DT <nphuongthuy1408@gmail.com>
2024-11-18 06:51:40 +0000

    update file

--- LittleLemonDB.sql
+++ LittleLemonDB.sql
@@ -1,15 +1,11 @@
 DELIMITER //
-CREATE PROCEDURE CancelBooking(IN booking_id INT)
+CREATE PROCEDURE UpdateBooking(IN booking_date DATE, IN booking_id INT)
 BEGIN
-DECLARE check_booking INT;
-SELECT COUNT(*) INTO check_booking FROM Bookings WHERE BookingID = booking_id;
-IF check_booking > 0 THEN
-DELETE FROM Bookings WHERE BookingID = booking_id;
-SELECT 'Booking is canceled' AS Status, booking_id AS BookingID;
-ELSE
-SELECT 'Booking not found' AS Status, booking_id AS BookingID;
-END IF;
+UPDATE Bookings
+SET BookingDate = booking_date
+WHERE BookingID = booking_id;
+SELECT 'booking is updated successfully' AS Status, booking_id AS BookingID;
 END //
 DELIMITER ;
 
-CALL CancelBooking(14);
+CALL UpdateBooking('2022-10-16',16);
